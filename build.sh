49c0f5995ad7fe340f7b7788c9e847b0f6e74ce9
kamikat <kamikat@banana.moe>
2018-02-06 18:40:07 +0800

    add build option CI_BUILD

--- build.sh
+++ build.sh
@@ -32,62 +32,27 @@ build() {
                 onbuild
 }
 
-# master
-build master 9.1 7 ubuntu16.04
-build master 9.0 7 ubuntu16.04
-build master 8.0 7 ubuntu16.04
-build master 8.0 6 ubuntu16.04
-build master 8.0 5 ubuntu16.04
-build master 8.0 7
-build master 8.0 6
-build master 8.0 5
-build master 7.5 6
-build master 7.5 5
-build master 7.5 4
-build master 7.5 3
-build master 7.0 4
+build_batch() {
+  build master $@
+  build 0.12.0 $@
+  build 0.11.0 $@
+  build  1.0.0 $@
+  if [ "$CI_BUILD" == "1" ]; then
+    docker push ${IMAGE_PREFIX:-""}mxnet-onbuild
+    docker images -q | xargs docker rmi
+  fi
+}
 
-# 1.0.0
-# build 1.0.0 9.1 7 ubuntu16.04
-# build 1.0.0 9.0 7 ubuntu16.04
-# build 1.0.0 8.0 7 ubuntu16.04
-# build 1.0.0 8.0 6 ubuntu16.04
-# build 1.0.0 8.0 5 ubuntu16.04
-# build 1.0.0 8.0 7
-# build 1.0.0 8.0 6
-# build 1.0.0 8.0 5
-# build 1.0.0 7.5 6
-# build 1.0.0 7.5 5
-# build 1.0.0 7.5 4
-# build 1.0.0 7.5 3
-# build 1.0.0 7.0 4
-
-# 0.11.0
-# build 0.11.0 9.1 7 ubuntu16.04
-# build 0.11.0 9.0 7 ubuntu16.04
-# build 0.11.0 8.0 7 ubuntu16.04
-# build 0.11.0 8.0 6 ubuntu16.04
-# build 0.11.0 8.0 5 ubuntu16.04
-# build 0.11.0 8.0 7
-# build 0.11.0 8.0 6
-# build 0.11.0 8.0 5
-# build 0.11.0 7.5 6
-# build 0.11.0 7.5 5
-# build 0.11.0 7.5 4
-# build 0.11.0 7.5 3
-# build 0.11.0 7.0 4
-
-# 0.12.0
-# build 0.12.0 9.1 7 ubuntu16.04
-# build 0.12.0 9.0 7 ubuntu16.04
-# build 0.12.0 8.0 7 ubuntu16.04
-# build 0.12.0 8.0 6 ubuntu16.04
-# build 0.12.0 8.0 5 ubuntu16.04
-# build 0.12.0 8.0 7
-# build 0.12.0 8.0 6
-# build 0.12.0 8.0 5
-# build 0.12.0 7.5 6
-# build 0.12.0 7.5 5
-# build 0.12.0 7.5 4
-# build 0.12.0 7.5 3
-# build 0.12.0 7.0 4
+build_batch 9.1 7 ubuntu16.04
+build_batch 9.0 7 ubuntu16.04
+build_batch 8.0 7 ubuntu16.04
+build_batch 8.0 6 ubuntu16.04
+build_batch 8.0 5 ubuntu16.04
+build_batch 8.0 7
+build_batch 8.0 6
+build_batch 8.0 5
+build_batch 7.5 6
+build_batch 7.5 5
+build_batch 7.5 4
+build_batch 7.5 3
+build_batch 7.0 4
