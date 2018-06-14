7cf32093ab9d19d28295e062d678c4002890ad99
kamikat <kamikat@banana.moe>
2018-06-14 18:43:49 +0800

    build mxnet from source

--- build.sh
+++ build.sh
@@ -16,23 +16,23 @@ build_base_image() {
                 $EXTRA_ARGS \
     --build-arg CUDA_VERSION=$1 \
     --build-arg CUDNN_VERSION=$2 \
+    --build-arg HTTP_PROXY=$http_proxy \
+    --build-arg HTTPS_PROXY=$https_proxy \
                 base${OS_VERSION##ubuntu} 1>&2
   echo >&1 $IMAGE_NAME
 }
 
 build() {
-  MXNET_VERSION=${1:-master}
-  CUDA_VERSION=${2:-8.0}
-  CUDNN_VERSION=${3:-5}
-  OS_VERSION=${4:-ubuntu14.04}
-  IMAGE_NAME=${IMAGE_PREFIX:-""}mxnet-onbuild:${MXNET_VERSION}-cu${CUDA_VERSION//./}-cudnn${CUDNN_VERSION//./}-${OS_VERSION}
+  CUDA_VERSION=${1:-8.0}
+  CUDNN_VERSION=${2:-5}
+  OS_VERSION=${3:-ubuntu14.04}
+  IMAGE_NAME=${IMAGE_PREFIX:-""}mxnet-onbuild:cu${CUDA_VERSION//./}-cudnn${CUDNN_VERSION//./}-${OS_VERSION}
   BASE_IMAGE=$(build_base_image $CUDA_VERSION $CUDNN_VERSION $OS_VERSION)
   echo >&2 "Building $IMAGE_NAME..."
   docker build \
     --tag       $IMAGE_NAME \
                 $EXTRA_ARGS \
     --build-arg BASE_IMAGE=$BASE_IMAGE \
-    --build-arg MXNET_VERSION=$MXNET_VERSION \
                 onbuild
 }
 
@@ -42,10 +42,7 @@ if [ "$CI_BUILD" == "1" -a -z "$TARGET" ]; then
 fi
 
 build_batch() {
-  build master $@
-  build 0.12.0 $@
-  build 0.11.0 $@
-  build  1.0.0 $@
+  build $@
   if [ "$CI_BUILD" == "1" ]; then
     docker push ${IMAGE_PREFIX:-""}mxnet-onbuild
     docker images ${IMAGE_PREFIX:-""}mxnet-onbuild -q | xargs docker rmi
